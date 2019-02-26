3fe0320c06ad78427989215f4c69a52fbdfa777b
Peter Keller <keller.f.peter@gmail.com>
2019-02-26 16:09:50 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(param)
+  while param > 0
+    puts "#{param} SECOND(S)!"
+    param -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(param)
+  while param > 0
+    puts "#{param} SECOND(S)!"
+    param -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
